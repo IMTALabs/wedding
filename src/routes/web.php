dd6735801309c17a17548b38e7311e6aca695ac4
pc <tienrider30@gmail.com>
2025-04-17 11:32:52 +0700

    feat: Login with google

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\Auth\GoogleController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('layout.layout');
 });
+
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
+
+Route::get('/login/google', [GoogleController::class, 'redirectToGoogle'])->name('login.google');
+Route::get('/login/google/callback', [GoogleController::class, 'handleGoogleCallback']);
