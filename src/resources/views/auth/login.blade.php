f71474ac1b6eabad54ad4981a5074540617fbbb3
pc <tienrider30@gmail.com>
2025-04-17 13:36:29 +0700

    feat: Login with google

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -1,13 +1,20 @@
 @extends('layout.guest')
 
 @section('content')
-    <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-        {{ __('Đăng nhập') }}
-    </h2>
-    <div class="text-center">
-        <a href="{{ route('login.google') }}" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-            Đăng nhập bằng Google
-        </a>
-    </div>
+    <div class="bg-gradient-to-br from-pink-100 to-rose-200 flex justify-center items-center min-h-screen">
+        <div class="bg-white p-10 rounded-xl shadow-2xl w-96 text-center">
+            <h2 class="text-3xl font-semibold text-gray-800 mb-8">RSVP with Google</h2>
 
+            <p class="text-gray-600 mb-4">Sign in with your Google account to RSVP and view wedding details.</p>
+
+            <a href="{{ route('login.google') }}"
+               class="w-full bg-red-300 hover:bg-red-400 text-white py-3 rounded-lg transition-colors duration-300 ease-in-out flex justify-center items-center space-x-4">
+                <svg width="30px" height="30px" viewBox="0 0 262.00 262.00" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" fill="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622 38.755 30.023 2.685.268c24.659-22.774 38.875-56.282 38.875-96.027" fill="#4285F4"></path><path d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055-34.523 0-63.824-22.773-74.269-54.25l-1.531.13-40.298 31.187-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1" fill="#34A853"></path><path d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82 0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602l42.356-32.782" fill="#FBBC05"></path><path d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0 79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251" fill="#EB4335"></path></g></svg>
+                <span class="font-medium">Sign in with Google</span>
+            </a>
+        </div>
+    </div>
+@endsection
+
+@section('js')
 @endsection
--- src/routes/web.php
+++ src/routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('layout.layout');
-});
+})->name('demo');
 
 Route::get('/login', function () {
     return view('auth.login');
