96c0dd3513e8fa742718b487dae6e46b102a7dcc
sky-quanph <quanph@skylab.vn>
2025-08-13 11:19:37 +0700

    fix: style

--- src/resources/views/guest_manager/album/create.blade.php
+++ src/resources/views/guest_manager/album/create.blade.php
@@ -1,12 +1,10 @@
-@extends('layout.'.$layout)
+@extends('layout.' . $layout)
 
 @section('content')
-
     <div class="intro-y flex flex-col sm:flex-row items-center mt-8">
         <h2 class="text-lg font-medium mr-auto">
             Thêm mới ảnh
         </h2>
-
     </div>
     <div class="pos intro-y grid grid-cols-12 gap-5 mt-5">
         <!-- BEGIN: Post Content -->
@@ -14,9 +12,7 @@
             <div class="post intro-y overflow-hidden box">
                 <div class="post__content tab-content">
                     <div id="content" class="tab-pane p-5 active" role="tabpanel" aria-labelledby="content-tab">
-                        <div class="border border-slate-200/60 dark:border-darkmode-400 rounded-md p-5 mt-5">
-                            <livewire:create-gallery-photo/>
-                        </div>
+                        <livewire:create-gallery-photo />
                     </div>
                 </div>
             </div>
@@ -24,17 +20,14 @@
         <!-- END: Post Content -->
         <!-- BEGIN: Post Info -->
         <div class="col-span-12 lg:col-span-4">
-
             <div class="intro-y box px-2">
-
-                <livewire:create-album/>
+                <livewire:create-album />
             </div>
         </div>
     </div>
 @endsection
 @push('scripts')
     <script>
-
         FilePond.registerPlugin(FilePondPluginImagePreview);
         FilePond.registerPlugin(FilePondPluginImageExifOrientation);
         FilePond.registerPlugin(FilePondPluginFileValidateType);
